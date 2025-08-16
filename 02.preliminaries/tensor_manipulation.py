766f992a336b5a3d1d93e39a3614daec17c756a9
Basantashrestha <basantashrestha@Basantas.local>
2025-08-16 19:37:33 +0545

    Binary sclar operation

--- 02.preliminaries/tensor_manipulation.py
+++ 02.preliminaries/tensor_manipulation.py
@@ -3,3 +3,16 @@ x = torch.arange(12, dtype=torch.float32)
 #unary operations like e*x 
 x = torch.exp(x)
 print(x)
+
+#binary scalar operations
+a = torch.tensor([1, 2, 3,4,5,6,7,8,9,10])
+b = torch.tensor([10, 9, 8,7,6,5,4,3,2,1])
+
+# operations 
+a+b, a-b, a*b, a/b, a**2
+
+print("addition:", a + b)
+print("subtraction:", a - b)
+print("multiplication:", a * b)
+print("division:", a / b)
+print("power:", a ** 2) 
