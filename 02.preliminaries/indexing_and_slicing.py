dfe478e02d38e2a209ad3972563e42ebc6d52b9d
Basantashrestha <basantashrestha@Basantas.local>
2025-08-16 19:22:59 +0545

    Replace rows element with values

--- 02.preliminaries/indexing_and_slicing.py
+++ 02.preliminaries/indexing_and_slicing.py
@@ -1,7 +1,6 @@
 import torch
 x = torch.arange(12, dtype=torch.float32)
 x = x.reshape(-1,4)
-print(x)
 #last row of the tensor
 print(x[-1])
 
@@ -12,4 +11,11 @@ print(x[1])
 print(x[:2])
 
 #to print specific element
-print(x[1,3])
+print(x[1,3]) 
+
+#remove second row and replace it with 22
+x[1] = 22
+
+#to replace first two rows with 88
+x[:2,:] = 88
+print(x)
