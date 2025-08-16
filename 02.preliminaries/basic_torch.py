ce620ad47400678892cbd0aec1a9285cd95e8104
Basantashrestha <basantashrestha@Basantas.local>
2025-08-16 16:58:34 +0545

    Reshape the tensor

--- 02.preliminaries/basic_torch.py
+++ 02.preliminaries/basic_torch.py
@@ -1,3 +1,15 @@
 import torch 
 x = torch.arange(12, dtype=torch.float32)
-print(x)
+
+#to see total number of elements
+print(x.numel())
+
+
+
+#reshaping the tensor 
+x = x.reshape(3, 4)
+
+#for shape of the tensor
+print(x.shape)
+
+print(x)    
