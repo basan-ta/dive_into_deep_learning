46b3f257e118d73237aa070308ab01b367add3ed
Basantashrestha <basantashrestha@Basantas.local>
2025-08-16 17:24:07 +0545

    indexing

---
+++ 02.preliminaries/indexing_and_slicing.py
@@ -0,0 +1,15 @@
+import torch
+x = torch.arange(12, dtype=torch.float32)
+x = x.reshape(-1,4)
+print(x)
+#last row of the tensor
+print(x[-1])
+
+#to print specific row
+print(x[1])
+
+#to print all rows and  specific column
+print(x[:2])
+
+#to print specific element
+print(x[1,3])
