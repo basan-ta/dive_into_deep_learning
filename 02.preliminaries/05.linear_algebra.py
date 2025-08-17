c26529c4ce2a0326acbeb2075bb541a33176998a
basan-ta <basanta.077bei008@ioepc.edu.no>
2025-08-17 12:56:31 +0545

    Vector manipulation and details check

--- 02.preliminaries/05.linear_algebra.py
+++ 02.preliminaries/05.linear_algebra.py
@@ -1,4 +1,16 @@
 import torch 
+#scalars
 x = torch.tensor(3.0)
 y = torch.tensor(7)
 print(x+y)
+
+#vectors (in L.algebra it is known as one-based indexing)
+a = torch.arange(3)
+print(f"vector a ",a)
+
+#access via indexing
+print(a[2]) #access second element of a ("2")
+
+#len and shape 
+print(f"length\n ",len(a))
+print(f"shape\n", a.shape)
